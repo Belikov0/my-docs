6fc32b4f2936dadf26404e620f60bf98799262db
zhangxiaofeng <zhangxiaofeng@endlessorigin.com>
2024-06-17 15:44:47 +0800

    chore: 拉取请求

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 function Person(name, age){
     this.name = name
     this.age = age
-    this.sayHi = function(){
-        console.log(`Hi, I am ${this.name}`)
-    }
+    
 }
-const person = new Object()
-Person.call(person, "John", 18)
-person.sayHi()
-console.log(person instanceof Person)
-console.log(person.constructor)
+
+Person.prototype.sayHi = function(){
+    console.log(`Hi, I am ${this.name}`)
+}
+
+function Student(name, age, id){
+    this.id = id
+    this.name = name
+    this.age = age
+}
+
+Student.prototype.__proto__ = Person.prototype
+
+
+const stu = new Student('张三', 15, 100)
+
+console.log(stu instanceof Person)
+stu.sayHi()
+
+console.log(stu.__proto__, Student.prototype.constructor, stu.constructor)
